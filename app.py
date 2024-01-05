e07a0fda18daa07e343afe28180e7c7bc0d581f2
RianaKartika <rianakartika03@gmail.com>
2024-01-05 16:16:28 +0700

    first commit

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def delete_restaurant():
 
 @app.route('/aboute')
 def aboute():
-    return render_template('aboutme.html')
+    return render_template('abouteme.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
